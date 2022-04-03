58a98cc1ff9f1d8fbe1d9ea4829b666d4bb45783
MaddoScientisto <maddo.science@gmail.com>
2022-04-03 23:52:59 +0200

    Added some dummy layout

--- LostWordTracker/Program.cs
+++ LostWordTracker/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,14 @@ namespace LostWordTracker
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
             await builder.Build().RunAsync();
         }
