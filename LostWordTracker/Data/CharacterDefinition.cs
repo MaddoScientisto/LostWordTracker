b93a813b53a5ddb8e7dbcb22ab5bc84ed33c4f4a
MaddoScientisto <maddo.science@gmail.com>
2022-04-12 19:40:20 +0200

    Added all Characters

--- LostWordTracker/Data/CharacterDefinition.cs
+++ LostWordTracker/Data/CharacterDefinition.cs
@@ -7,5 +7,11 @@
         public string Universe { get; set; }
         public string Portrait { get; set; }
         public string Tier { get; set; }
+        public int TierRank { get; set; }
+        public string FarmTier { get; set; }
+        public string CqTier { get; set; } 
+
+        public string ObtainType { get; set; }
+        
     }
 }
