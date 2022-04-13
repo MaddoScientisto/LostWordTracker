12fd4eda9e3175cac86eca453e5b2ecd691665d4
MaddoScientisto <maddo.science@gmail.com>
2022-04-13 14:59:03 +0200

    Finished unit tests

--- LostWordTracker.Test/UnitTest1.cs
+++ LostWordTracker.Test/UnitTest1.cs
@@ -16,6 +16,7 @@ using System.Threading;
 using System.Net;
 using System.IO;
 using FluentAssertions;
+using System.Linq;
 
 namespace LostWordTracker.Test
 {
@@ -72,7 +73,7 @@ namespace LostWordTracker.Test
         }
 
         [Test]
-        public async Task LoadCharacters()
+        public async Task LoadCharacterDefinitions()
         {
 
             var dataService = _services.GetRequiredService<IDataService>();
@@ -80,12 +81,102 @@ namespace LostWordTracker.Test
             //var mockedData = new Mock<IDataService>
 
             var characters = await dataService.LoadCharacterDefinitions();
-            
+
             characters.Should().NotBeNull();
 
-            characters.Characters.Should().NotBeNull().And.ContainKeys(new int[] { 1, 2 });
+            characters.Characters.Should().NotBeNullOrEmpty().And.ContainKeys(new int[] { 1, 2 });
+
+            characters.CharacterStorage.Should().BeNull();
+
+        }
+
+        [Test]
+        public async Task LoadCharacterData()
+        {
+
+            var dataService = _services.GetRequiredService<IDataService>();
+
+            var characters = await dataService.GetCharactersData();
+
+            characters.Should().NotBeNull();
+
+            characters.Characters.Should().NotBeNullOrEmpty().And.ContainKeys(new int[] { 1, 2 });
+
+            characters.CharacterStorage.Should().NotBeNullOrEmpty().And.ContainEquivalentOf(new CharacterStorage()
+            {
+                Id = 1,
+                Level = 0,
+                Obtained = false,
+                LimitBreak = 0
+            });
 
 
         }
+
+        [Test]
+        public async Task SaveLoadData()
+        {
+            var dataService = _services.GetRequiredService<IDataService>();
+
+            var dataToSave = new System.Collections.Generic.List<CharacterStorage>()
+                {
+                     new CharacterStorage()
+            {
+                Id = 1,
+                Level = 100,
+                LimitBreak = 5,
+                Obtained = true
+            }
+        };
+
+
+
+
+            var testChars = new CharacterDefinitions()
+            {
+                //Characters = new System.Collections.Generic.Dictionary<int, CharacterDefinition>
+                //{
+                //    {1, new CharacterDefinition() }
+                //},
+                CharacterStorage = dataToSave
+            };
+
+            await dataService.SaveData(testChars);
+
+            var data = await dataService.LoadData();
+
+            data.Characters.Should().NotBeNullOrEmpty().And.ContainKeys(new int[] { 1, 2 });
+            data.CharacterStorage.Should().NotBeNullOrEmpty().And.ContainEquivalentOf(dataToSave.First());
+        }
+
+        [Test]
+        public async Task ImportExportData()
+        {
+            var dataService = _services.GetRequiredService<IDataService>();
+
+            string dataString = "[{\"Id\":1,\"Obtained\":true,\"Level\":100,\"LimitBreak\":5},{\"Id\":2,\"Obtained\":false,\"Level\":0,\"LimitBreak\":0}]";
+            var data = await dataService.Import(dataString);
+
+            data.Should().NotBeNull();
+
+            data.CharacterStorage.Should().NotBeNullOrEmpty().And.ContainEquivalentOf(new CharacterStorage()
+            {
+                Id = 1,
+                Level = 100,
+                LimitBreak = 5,
+                Obtained = true
+            }).And.ContainEquivalentOf(new CharacterStorage()
+            {
+                Id = 2,
+                Obtained = false,
+                Level = 0,
+                LimitBreak = 0
+            });
+
+            var exportedData = dataService.Export(data);
+            exportedData.Should().NotBeNull().And.BeEquivalentTo(dataString);
+        }
+
+   
     }
 }
